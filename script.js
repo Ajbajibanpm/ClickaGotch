9e29ff0e9cb1252e9b47ed7637f79e8fdf3223a3
Ajbajibanpm <elenadivino@gmail.com>
2025-12-05 17:17:58 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+
+let variabile = "valoreCheCambia"; //dichiarazione variabile mutabile
+const Variabile = "VALORE_FISSO"; //dichiarazione variabile costante
+console.log('Variabile');
+console.log("Ciaooone");
+
+let button = document.getElementById('off');
+
+button.addEventListener('click',showMessages)
+
+//#FUNZIONI UTILI
+//Funzioni per mostrare i messaggi
+function showMessages(){
+if (button.id === "off") {button.id = "on";
+    button.innerHTML =`<img src="b.png">`} 
+else {button.id = "off"; button.innerHTML =`<i class="bi bi-truck-front-fill"></i>`}
+}
